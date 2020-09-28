1b298b9b4c7701ca3e98399bdca26fc2c77898d2
zamharrad <mhmdzamhar@gamil.com>
2020-09-28 18:54:47 +0530

    added roundScore and dice image changes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+ - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
@@ -15,11 +14,30 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).  innerHTML = "<em>" + dice + "</em>";
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
 document.querySelector(".dice").style.display = "none";
 
-//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 0.1 Random number
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  // 0.2 Display the result
+  document.querySelector(".dice").style.display = "block";
+  document.querySelector(".dice").src = "dice-" + dice + ".png";
+
+  // 0.3 Update the round score if the rolled number was NOT a 1
+
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  }
+});
